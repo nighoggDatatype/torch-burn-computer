e94561f6d1b719692a5e304cbb1f5508f818f5e8
nighoggDatatype <hosherman1965@gmail.com>
2026-07-08 21:53:25 +0800

    Add tooltip to ButtonArray

--- src/components/ButtonArray.tsx
+++ src/components/ButtonArray.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+import Tooltip from "./Tooltip.js"
 
 
 type ButtonConfig = {
@@ -9,24 +11,30 @@ type ButtonConfig = {
         background: string
     } | {}
 }
-
 function ButtonArray(
-    { value, setValue, buttonList } : 
-    { value: string, setValue: (value: string) => void, buttonList: ButtonConfig[] }) {
-  return (
-    <div
-      style={{ display: 'flex', alignItems: 'center', gap: 8, marginLeft: 118, marginBottom: 8 }}
-    >
-        {buttonList.map((buttonConfig, _) => {
-            return <button
-            className={`bc-unit-btn${buttonConfig.value === value ? ' active' : ''}`}
-            onClick={() => setValue(buttonConfig.value)}
-            style={ buttonConfig.value === value ? buttonConfig.style : {}}
-            >
-                {buttonConfig.label}
-            </button>
-        })}
+    { label = null, tooltip = null, value, setValue, buttonList } : 
+    { label? : string | null, tooltip? : string | null, value: string, setValue: (value: string) => void, buttonList: ButtonConfig[] }) {
+    const buttonArray = buttonList.map((buttonConfig, _) => {
+        return <button
+        className={`bc-unit-btn${buttonConfig.value === value ? ' active' : ''}`}
+        onClick={() => setValue(buttonConfig.value)}
+        style={ buttonConfig.value === value ? buttonConfig.style : {}}
+        >
+            {buttonConfig.label}
+        </button>
+    })
+    return label === null ? (
+    <div style={{ display: 'flex', alignItems: 'center', gap: 8, marginLeft: 118, marginBottom: 8 }}>
+    {buttonArray}
     </div>
-  );
+    ) : (
+    <div className="bc-input-row">
+      <label className="bc-label" htmlFor={React.useId()} style={{ display: 'flex', alignItems: 'center' }}>
+        <span style={{ flex: 1 }}>{label}</span>
+        {tooltip !== null && (<Tooltip label={label} desc={tooltip} img=""/>)}
+      </label>
+      {buttonArray}
+    </div>
+    );
 }
 export default ButtonArray;
